776f3245d5c8afda075c66a10d6307b897cea794
Emma Persson <emma.e.persson@me.com>
2015-02-02 14:11:52 +0100

    Lagt till mitt namn

--- BinarySearchTreeNode.java
+++ BinarySearchTreeNode.java
@@ -1,20 +1,21 @@
 package alda.tree;
 
 /**
- * Denna klass representerar noderna i ett binärt sökträd utan dubletter.
+ * Denna klass representerar noderna i ett binï¿½rt sï¿½ktrï¿½d utan dubletter.
  * 
- * Detta är den enda av de tre klasserna ni ska göra några ändringar i. (Om ni
- * inte vill lägga till fler testfall.) De ändringar som är tillåtna är dock
- * begränsade av följande regler:
+ * Detta ï¿½r den enda av de tre klasserna ni ska gï¿½ra nï¿½gra ï¿½ndringar i. (Om ni
+ * inte vill lï¿½gga till fler testfall.) De ï¿½ndringar som ï¿½r tillï¿½tna ï¿½r dock
+ * begrï¿½nsade av fï¿½ljande regler:
  * <ul>
- * <li>Ni får INTE lägga till några fler instansvariabler.
- * <li>Ni får INTE lägga till några statiska variabler.
- * <li>Ni får INTE använda några loopar någonstans.
- * <li>Ni FÅR lägga till fler metoder, dessa ska då vara privata.
+ * <li>Ni fï¿½r INTE lï¿½gga till nï¿½gra fler instansvariabler.
+ * <li>Ni fï¿½r INTE lï¿½gga till nï¿½gra statiska variabler.
+ * <li>Ni fï¿½r INTE anvï¿½nda nï¿½gra loopar nï¿½gonstans.
+ * <li>Ni Fï¿½R lï¿½gga till fler metoder, dessa ska dï¿½ vara privata.
  * </ul>
  * 
  * @author henrikbe
  * @author Elise Edette (tero0337) tero0337@student.su.se
+ * @author Emma Persson (empe5691)
  * 
  * @param <T>
  */
@@ -29,46 +30,46 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
 	}
 
 	/**
-	 * Lägger till en nod i det binära sökträdet. Om noden redan existerar så
-	 * lämnas trädet oförändrat.
+	 * Lï¿½gger till en nod i det binï¿½ra sï¿½ktrï¿½det. Om noden redan existerar sï¿½
+	 * lï¿½mnas trï¿½det ofï¿½rï¿½ndrat.
 	 * 
 	 * @param data
-	 *            datat för noden som ska läggas till.
-	 * @return true om en ny nod lades till trädet.
+	 *            datat fï¿½r noden som ska lï¿½ggas till.
+	 * @return true om en ny nod lades till trï¿½det.
 	 */
 	public boolean add(T data) {
 		return false;
 	}
 
 	/**
-	 * Privat hjälpmetod som är till nytta vid borttag. Ni behöver inte
+	 * Privat hjï¿½lpmetod som ï¿½r till nytta vid borttag. Ni behï¿½ver inte
 	 * skriva/utnyttja denna metod om ni inte vill.
 	 * 
-	 * @return det minsta elementet i det (sub)träd som noden utgör root i.
+	 * @return det minsta elementet i det (sub)trï¿½d som noden utgï¿½r root i.
 	 */
 	private T findMin() {
 		return null;
 	}
 
 	/**
-	 * Tar bort ett element ur trädet. Om elementet inte existerar s lämnas
-	 * trädet oförändrat.
+	 * Tar bort ett element ur trï¿½det. Om elementet inte existerar s lï¿½mnas
+	 * trï¿½det ofï¿½rï¿½ndrat.
 	 * 
 	 * @param data
-	 *            elementet som ska tas bort ur trädet.
-	 * @return en referens till nodens subträd efter borttaget.
+	 *            elementet som ska tas bort ur trï¿½det.
+	 * @return en referens till nodens subtrï¿½d efter borttaget.
 	 */
 	public BinarySearchTreeNode<T> remove(T data) {
 		return null;
 	}
 
 	/**
-	 * Kontrollerar om ett givet element finns i det (sub)träd som noden utgör
+	 * Kontrollerar om ett givet element finns i det (sub)trï¿½d som noden utgï¿½r
 	 * root i.
 	 * 
 	 * @param data
-	 *            det sökta elementet.
-	 * @return true om det sökta elementet finns i det (sub)träd som noden utgör
+	 *            det sï¿½kta elementet.
+	 * @return true om det sï¿½kta elementet finns i det (sub)trï¿½d som noden utgï¿½r
 	 *         root i.
 	 */
 	public boolean contains(T data) {
@@ -76,16 +77,16 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
 	}
 
 	/**
-	 * Storleken på det (sub)träd som noden utgör root i.
+	 * Storleken pï¿½ det (sub)trï¿½d som noden utgï¿½r root i.
 	 * 
-	 * @return det totala antalet noder i det (sub)träd som noden utgör root i.
+	 * @return det totala antalet noder i det (sub)trï¿½d som noden utgï¿½r root i.
 	 */
 	public int size() {
 		return 0;
 	}
 
 	/**
-	 * Det högsta djupet i det (sub)träd som noden utgör root i.
+	 * Det hï¿½gsta djupet i det (sub)trï¿½d som noden utgï¿½r root i.
 	 * 
 	 * @return djupet.
 	 */
@@ -94,11 +95,11 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
 	}
 
 	/**
-	 * Returnerar en strängrepresentation för det (sub)träd som noden utgör root
-	 * i. Denna representation består av elementens dataobjekt i sorterad
+	 * Returnerar en strï¿½ngrepresentation fï¿½r det (sub)trï¿½d som noden utgï¿½r root
+	 * i. Denna representation bestï¿½r av elementens dataobjekt i sorterad
 	 * ordning med ", " mellan elementen.
 	 * 
-	 * @return strängrepresentationen för det (sub)träd som noden utgör root i.
+	 * @return strï¿½ngrepresentationen fï¿½r det (sub)trï¿½d som noden utgï¿½r root i.
 	 */
 	public String toString() {
 		return "";
